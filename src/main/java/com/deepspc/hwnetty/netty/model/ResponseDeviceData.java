5c1b0f9f11302c98a05fc6732403663b1443f385
didoguan@163.com <didoguan@163.com>
2020-04-23 14:00:07 +0800

    创建向终端传输数据的对象实体

---
+++ src/main/java/com/deepspc/hwnetty/netty/model/ResponseDeviceData.java
@@ -0,0 +1,31 @@
+package com.deepspc.hwnetty.netty.model;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @Description 向终端传送数据对象
+ * @Author didoguan
+ * @Date 2020/4/23
+ **/
+@Data
+public class ResponseDeviceData implements Serializable {
+
+    private static final long serialVersionUID = -876983669125082693L;
+
+    //唯一码
+    private String uniqueNo;
+    //序列号
+    private String serialNo;
+    //温度
+    private Float temperature;
+    //开始时间
+    private String startTime;
+    //结束时间
+    private String endTime;
+
+    public ResponseDeviceData() {
+
+    }
+}
